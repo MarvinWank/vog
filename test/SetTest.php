7e99851bb07ca1570edfa32d757491eaa515cd57
dbenkert <mail@dirk-benkert.de>
2021-01-05 20:50:51 +0100

    added test for set equals.

--- test/SetTest.php
+++ test/SetTest.php
@@ -60,4 +60,47 @@ class SetTest extends VogTestCase
         $this->assertEquals(2, $set->count());
         $this->assertFalse($set->contains($new));
     }
+
+    /**
+     * @test
+     */
+    public function it_tests_set_equals() {
+        $data = [
+            Recipe::fromArray([
+                'title' => 'Omni',
+                'minutesToPrepare' => 20,
+                'rating' => 5,
+                'dietStyle' => DietStyle::OMNIVORE()
+            ]),
+            Recipe::fromArray([
+                'title' => 'Vegan',
+                'minutesToPrepare' => 20,
+                'rating' => 5,
+                'dietStyle' => DietStyle::VEGAN()
+            ]),
+            Recipe::fromArray([
+                'title' => 'Vegetarian',
+                'minutesToPrepare' => 20,
+                'rating' => 5,
+                'dietStyle' => DietStyle::VEGETARIAN()
+            ])
+        ];
+
+        $set1 = RecipeSet::fromArray($data);
+        $set2 = RecipeSet::fromArray($data);
+
+        $result = $set1->equals($set2);
+        $this->assertTrue(is_bool($result));
+        $this->assertTrue($result);
+
+        $set2->remove(Recipe::fromArray([
+            'title' => 'Vegetarian',
+            'minutesToPrepare' => 20,
+            'rating' => 5,
+            'dietStyle' => DietStyle::VEGETARIAN()
+        ]));
+
+        $result = $set1->equals($set2);
+        $this->assertFalse($result);
+    }
 }
