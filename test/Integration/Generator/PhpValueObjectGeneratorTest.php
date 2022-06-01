0d6e1e5782694305e28ad088da6a82930ae88b8a
marvin <marvin.wank@iu.org>
2022-06-01 13:03:28 +0200

    tests interface generation from php class generators

--- test/Integration/Generator/PhpValueObjectGeneratorTest.php
+++ test/Integration/Generator/PhpValueObjectGeneratorTest.php
@@ -4,6 +4,7 @@ namespace Vog\Test\Integration\Generator;
 
 use Integration\IntegrationTestCase;
 use Vog\Generator\Php\Classes\PhpValueObjectClassGenerator;
+use Vog\Generator\Php\Interfaces\ValueObjectInterfaceGenerator;
 use Vog\ValueObjects\VogDefinition;
 use Vog\ValueObjects\VogTypes;
 
@@ -62,5 +63,33 @@ class PhpValueObjectGeneratorTest extends IntegrationTestCase
         $phpcode = $generator->getCode();
 
         $this->assertStringContainsString('final class TestClass extends ParentClass implements FooInterface, BarInterface', $phpcode);
+    }
+
+    public function testGenerateInterface()
+    {
+        $definition = new VogDefinition(
+            'TestClass',
+            'app/DTOs',
+            VogTypes::valueObject(),
+            [
+                'title' => 'string',
+                'amount' => 'int',
+                'active' => 'bool'
+            ],
+            'title',
+            'Y-m-d',
+            null,
+            'ParentClass',
+            null,
+            true,
+            null
+        );
+        $config = $this->getDummyConfiguration();
+
+        $generator = new PhpValueObjectClassGenerator($definition, $config->getGeneratorOptions(), 'Vog\Test\TestObjects');
+        $actual = $generator->getInterfaceCode();
+        $expected = file_get_contents(__DIR__ . '/expected/ValueObjectInterface.vogtest');
+
+        $this->assertEquals($expected, $actual);
     }
 }
